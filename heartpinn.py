fc4e60f372ee5cff110e564c83d300da18a92579
Adam Jakobsen <60152956+Adamjakobsen@users.noreply.github.com>
2023-05-13 13:33:25 +0200

    Update heartpinn.py
    Fixed issue with scaling features. Minmax yields much better results

--- heartpinn.py
+++ heartpinn.py
@@ -68,27 +68,38 @@ class PINN():
     def get_data(self):
         from utils import get_data, get_boundary
         from sklearn.preprocessing import StandardScaler
-        scaler = StandardScaler()
+        from sklearn.preprocessing import MinMaxScaler
+        scaler = MinMaxScaler()
         vertices, triangles, vm = get_data()
         # normalize features
-        x_mean = 0  # np.mean(vertices[:, 0])
-        x_std = 1  # np.std(vertices[:, 0])
-        y_mean = 0  # np.mean(vertices[:, 1])
-        y_std = 1  # np.std(vertices[:, 1])
-        vertices[:, 0] = (vertices[:, 0] - x_mean) / x_std
-        vertices[:, 1] = (vertices[:, 1] - y_mean) / y_std
+        # x_mean = np.mean(vertices[:, 0])
+        # x_std = np.std(vertices[:, 0])
+        # y_mean = np.mean(vertices[:, 1])
+        # y_std = np.std(vertices[:, 1])
+        print("x_max, x_min:", np.max(vertices[:, 0]), np.min(vertices[:, 0]))
+        print("y_max, y_min:", np.max(vertices[:, 1]), np.min(vertices[:, 1]))
+        print("x_mean:", np.mean(vertices[:, 0]))
+        # vertices = vertices
+        # (vertices[:, 0] - x_mean) / x_std
+        # vertices[:, 0] = scaler.fit_transform(vertices[:, 0])
+        # (vertices[:, 1] - y_mean) / y_std
+        # vertices[:, 1] = scaler.fit_transform(vertices[:, 1])
 
         self.vertices = vertices
         self.triangles = triangles
 
         print("self.triangles shape:", self.triangles.shape)
-        self.vm = vm[:20, :]
-        x = vertices[:, 0]
-        y = vertices[:, 1]
-        t = np.linspace(0, 600, 121)[:20]
-        t_mean = 0  # np.mean(t)
-        t_std = 1  # np.std(t)
-        t = (t - t_mean) / t_std
+        self.vm = vm[:10, ::2]
+        x = vertices[::2, 0]
+        y = vertices[::2, 1]
+        t = np.linspace(0, 600, 121)[:10]
+        # t_mean = np.mean(t)
+        # t_std = np.std(t)
+        # t = scaler.fit_transform(t)  # (t - t_mean) / t_std
+
+        print("-", np.max(x), np.min(x))
+        print("-", np.max(y), np.min(y))
+        print("-", np.max(t), np.min(t))
 
         X, T = np.meshgrid(x, t)
         Y, T = np.meshgrid(y, t)
@@ -130,7 +141,7 @@ class PINN():
 
         T_ic = observe_train[:, -1].reshape(-1, 1)
 
-        idx_init = np.where(np.isclose(T_ic, 5, atol=5))[0]
+        idx_init = np.where(np.isclose(T_ic, 5, rtol=1))[0]
         v_init = v_train[idx_init]
         observe_init = observe_train[idx_init]
 
@@ -160,19 +171,22 @@ def main():
 
     X, X_boundary, v = pinn.get_data()
     X_train, X_test, v_train, v_test = train_test_split(
-        X, v, test_size=0.1)
-
+        X, v, test_size=0.8)
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    v_train = scaler.fit_transform(v_train.reshape(-1, 1))
     data_list = [X, X_train, v_train, v]
 
     geomtime = pinn.geotime()
     observe_v = dde.PointSetBC(X_train, v_train, component=0)
     ic = pinn.IC(X_train, v_train)
     bc = pinn.BC(geomtime)
-    input_data = [bc, observe_v]
+    input_data = [bc, ic, observe_v]
     data = dde.data.TimePDE(geomtime,
                             pinn.pde2d,
                             input_data,
-                            num_domain=40000,
+                            num_domain=30000,
                             num_boundary=100)
     """
 
@@ -203,23 +217,31 @@ def main():
                             input_data,
                             anchors=X_train)
     """
-    n_neurons = 121
-    n_layers = 6
+    n_neurons = 50
+    n_layers = 4
     n_epochs = 6000
-    activations = ["sinc", "sin", "sin",
-                   "sin", "sin", "sin", "sin"]
+    activations = ["tanh", "tanh", "tanh",
+                   "tanh", "tanh", "tanh"]
+    net = dde.maps.FNN([3] + [100] + n_layers * [n_neurons] +
+                       [2], activations, "Glorot normal")
+    # net.regularizer = ("l2", 0.1)
 
+    # silu: not too bad, but not great 101x5
+    # tanh: not too bad, but not great 100x5
+    """  
+    n_neurons = 100
+    n_layers = 5
+    n_epochs = 6000
     net = dde.maps.FNN([3] + n_layers * [n_neurons] +
-                       [2], activations, "He normal")
-
-    # silu: not too bad, but not great
+                       [2], "tanh", "Glorot normal")
+    """
 
 # Set model_save_path to save the model
     import os
     save_path = os.getcwd()+f"/models/heart_model_{n_neurons}x{n_layers}"
 
     if sys.argv[1] == "train":
-        init_weights = [0, 0, 0, 1]
+        init_weights = [0, 0, 0, 1, 0]
         checker = dde.callbacks.ModelCheckpoint(
             save_path, save_better_only=True, period=2000)
 
@@ -256,13 +278,19 @@ def main():
         dde.saveplot(losshistory, train_state, issave=True, isplot=True)
         """
         # Phase 2
+        weights_phase2 = [0, 0, 0, 0, 1]
+        model.compile("adam", lr=0.0005, loss_weights=weights_phase2)
+        losshistory, train_state = model.train(
+            iterations=20000, model_save_path=save_path)
+        dde.saveplot(losshistory, train_state, issave=True, isplot=True)
 
-        model.compile("adam", lr=0.0005)
+        # Phase 3
+        weights_phase3 = [1, 1, 2, 1, 1]
+        model.compile("adam", lr=0.0005, loss_weights=weights_phase3)
         losshistory, train_state = model.train(
             iterations=50000, model_save_path=save_path)
         dde.saveplot(losshistory, train_state, issave=True, isplot=True)
 
-        # Phase 3
         model.compile("L-BFGS-B")
 
         losshistory, train_state = model.train(
@@ -293,7 +321,7 @@ def main():
 
 
 if __name__ == "__main__":
-
+    dde.config.set_random_seed(42)
     torch.cuda.set_per_process_memory_fraction(0.9)
     dde.config.set_default_float("float16")
 
